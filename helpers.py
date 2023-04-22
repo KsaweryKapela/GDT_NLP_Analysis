e478d56c671ad0339406f004cf89dd2ec360f383
KsaweryKapela <ksawerykapela97@gmail.com>
2023-04-22 03:16:26 +0200

    Tryed some shallow clfs

--- helpers.py
+++ helpers.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import torch
+import numpy as np
 
 def open_and_prepare_df(ds):
 
@@ -12,6 +13,9 @@ def open_and_prepare_df(ds):
         df = df[df['time'] > 300]
         df = df[df['label'] != 1]
 
+    elif ds == 'features':
+        df = pd.read_excel(io=f'{path}NLP_FEATURES.xlsx')
+
     for item in [f'nlp_{i}' for i in range(2, 6)]:
         df = df[df[item].apply(lambda x: len(x) > 10)]
 
@@ -20,3 +24,16 @@ def open_and_prepare_df(ds):
 def set_device():
     print(f'Cuda available: {torch.cuda.is_available()}')
     return "cuda:0" if torch.cuda.is_available() else "cpu"
+    
+
+def X_y_split(df, X_string):
+    
+    X = []
+    X_raw = df[X_string].values
+    for item in X_raw:
+        X.append(eval(item))
+
+    X = np.asarray(X)
+    y = df['label'].values
+
+    return X, y
